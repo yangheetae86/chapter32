caae82df5d9df887fe7886a8d7de7e1d69b2fdfb
heroparkjisung2 <heroparkjisung2@gmail.com>
2020-06-19 09:18:59 +0900

    Initial Commit

---
+++ chapter32/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  chapter32
+//
+//  Created by grinbi on 2020/06/19.
+//  Copyright © 2020 yht. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
